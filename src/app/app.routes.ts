50072a1f05b64857332d037301b15128cf136bb4
axelvrgn <axel.vergognan@protonmail.com>
2024-10-11 09:25:58 +0200

    intial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,29 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: 'register',
+    loadComponent: () =>
+      import('./register/register.page').then((m) => m.RegisterPage),
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then((m) => m.LoginPage),
+  },
+  {
+    path: 'contact',
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./contact/contact.page').then((m) => m.ContactPage),
+      },
+      {
+        path: 'new',
+        loadComponent: () =>
+          import('./contact/new-contact/new-contact.page').then(
+            (m) => m.NewContactPage
+          ),
+      },
+    ],
   },
 ];
